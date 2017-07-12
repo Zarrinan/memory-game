1ccd9e7d5d79b905bfa716a7d78f6aa86d5121ab
Zarrina Niyazova <zarrina_@hotmail.com>
2017-07-11 22:54:44 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
-cards [0] = cardOne;
-cardsInPlay.push("cardOne");
-console.log("User flipped queen");
-var cardTwo;
-cards [2] = cardTwo;
-cardsInPlay.push("cardTwo");
-console.log("User flipper king");
-if (cardsInPlay [0] === cardsInPlay [1]) {
-alert("You found a match!");
-} else  {
-  alert("Sorry, try again.")
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  };
+};
+var flipCard = function (cardId) {
+console.log("User flipped " +  cards[cardId]);
+cardsInPlay.push("cards[cardId]"); {
+checkForMatch();
 }
-
-
+};
+flipCard(0);
+flipCard(2);
